201661b2f3821833010afd7364e2a055dc3016c7
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-08-03 23:46:44 -0500

    Added getUnexpiredTokens db query

--- backend/src/db/queries/token.ts
+++ backend/src/db/queries/token.ts
@@ -1,11 +1,27 @@
 import { and, between, count, eq, sql } from "drizzle-orm";
 
 import { db } from "../db";
-import { SelectToken, tokensTable } from "../schema";
+import { tokensTable } from "../schema";
 
 export async function getUnexpiredTokensCount(tokenId: number) {
   return await db
     .select({ count: count() })
+    .from(tokensTable)
+    .where(
+      and(
+        eq(tokensTable.id, tokenId),
+        between(
+          tokensTable.created_at,
+          sql`now() - interval '30 minutes'`,
+          sql`now()`
+        )
+      )
+    );
+}
+
+export async function getUnexpiredTokens(tokenId: number) {
+  return await db
+    .select()
     .from(tokensTable)
     .where(
       and(
