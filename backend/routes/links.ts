b6e2c524f2c5d9b963ded501732b3ec9ec2a8823
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-08-12 23:57:09 -0500

    Added add link api route and db query

--- backend/routes/links.ts
+++ backend/routes/links.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import ApiResponse from "../types/ApiResponse.type";
 import Link from "../types/link.type";
 import { ErrorCodes } from "../constants/errors";
-import { getLinksByLockerId } from "../src/db/queries/links";
+import { addLink, getLinksByLockerId } from "../src/db/queries/links";
 
 const router = Router();
 
@@ -53,6 +53,72 @@ router.get("/:id", async (req, res) => {
     };
   }
   res.json(resp);
+});
+
+// post
+
+router.post("/add", async (req, res) => {
+  /**
+   * Passing the new locker object in the url, a new link is added and the new id is returned.
+   * Request body: {
+   *   url: string,
+   *   name: string,
+   *   tags: string[],
+   *   lockerId: number
+   * }
+   * Response: {
+   *   success: true,
+   *   payload: number
+   * } | {
+   *   success: false,
+   *   errorCode: number,
+   *   errorMessage: string
+   * }
+   */
+  let resp: ApiResponse<number>;
+
+  if (
+    typeof req.body.name !== "number" ||
+    typeof req.body.url !== "string" ||
+    !(req.body.tags instanceof Array) ||
+    typeof req.body.lockerId !== "number"
+  ) {
+    resp = {
+      success: false,
+      errorCode: ErrorCodes.IncorrectRequest,
+      errorMessage: "Incorrect request format.",
+    };
+    res.json(resp);
+    return;
+  }
+
+  try {
+    const addLinkResp = await addLink({
+      url: req.body.url,
+      name: req.body.name,
+      locker_id: req.body.lockerId,
+    });
+    if (addLinkResp.length === 0) {
+      resp = {
+        success: false,
+        errorCode: ErrorCodes.CouldNotAddNewLink,
+        errorMessage: "New link could not be added.",
+      };
+    } else {
+      resp = {
+        success: true,
+        payload: addLinkResp[0].newLinkId,
+      };
+    }
+  } catch (e) {
+    resp = {
+      success: false,
+      errorCode: ErrorCodes.UnexpectedErrorRaised,
+      errorMessage: `An unexpected error was raised while adding link into db: ${e}`,
+    };
+  }
+  res.json(resp);
+  return;
 });
 
 export { router as linksRoutes };
--- backend/src/db/queries/links.ts
+++ backend/src/db/queries/links.ts
@@ -1,7 +1,7 @@
 import { and, eq } from "drizzle-orm";
 
 import { db } from "../db";
-import { SelectLink, linksTable } from "../schema";
+import { InsertLink, SelectLink, linksTable } from "../schema";
 
 export async function getLinksByLockerId(
   lockerId: SelectLink["locker_id"]
@@ -11,4 +11,15 @@ export async function getLinksByLockerId(
     .from(linksTable)
     .where(eq(linksTable.locker_id, lockerId || -1))
     .limit(50);
+}
+
+export async function addLink(locker: {
+  name: InsertLink["name"];
+  url: InsertLink["url"];
+  locker_id: InsertLink["locker_id"];
+}) {
+  return await db
+    .insert(linksTable)
+    .values(locker)
+    .returning({ newLinkId: linksTable.id });
 }
