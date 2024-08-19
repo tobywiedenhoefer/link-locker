a89adaf94e775fd2fda5081e0363aa916e9debf0
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-08-18 23:17:31 -0500

    Updated to logout when cache is expired. Implemented worlfow to keep requests from being sent.

--- frontend/src/pages/Lockers/Lockers.tsx
+++ frontend/src/pages/Lockers/Lockers.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import LockerCard from "../../components/LockerCard/LockerCard";
 import SearchLockedLockersButton from "../../components/SearchLockedLockersButton/SearchLockedLockersButton";
@@ -7,21 +8,37 @@ import AddLockerButton from "../../components/AddLockerButton/AddLockerButton";
 import Locker from "../../types/locker.type";
 
 import { getLockers } from "../../store/data.store";
+import { ErrorCodes } from "../../shared/errors";
+import SubmissionWorkflow from "../../constants/submissionWorkflows";
 
 import "./Lockers.css";
 
 export default function Lockers() {
   const [lockers, setLockers] = useState<Array<Locker>>([]);
+  const [workflow, setWorkflow] = useState<SubmissionWorkflow>(
+    SubmissionWorkflow.default
+  );
+  const navigate = useNavigate();
   useEffect(() => {
     (async () => {
-      const resp = await getLockers();
-      if (resp.success) {
-        setLockers(resp.payload);
-      } else {
-        console.error("Error:", resp.errorMessage);
+      switch (workflow) {
+        case SubmissionWorkflow.default: {
+          const resp = await getLockers();
+          if (resp.success) {
+            setLockers(resp.payload);
+            setWorkflow(SubmissionWorkflow.success);
+          } else if (resp.errorCode === ErrorCodes.CacheExpiredOrNotSet) {
+            console.log("Cache not set, logging out...");
+            navigate("/logout");
+          } else {
+            console.error("unknown error:", resp);
+            setWorkflow(SubmissionWorkflow.failure);
+          }
+          break;
+        }
       }
     })();
-  }, [lockers]);
+  }, [workflow, lockers]);
   const handleNewLockerSubmission = (locker: Locker) => {
     setLockers([...lockers, locker]);
   };
