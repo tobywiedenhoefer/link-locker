0260fc6916935a61e03f9001345bb59615bf42e5
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-08-30 23:33:14 -0500

    Added UI for removal workflow

--- frontend/src/components/LinkCard/LinkCard.tsx
+++ frontend/src/components/LinkCard/LinkCard.tsx
@@ -1,24 +1,55 @@
+import { useEffect, useState } from "react";
+import { IconCheck, IconX } from "@tabler/icons-react";
+
 import Link from "../../types/link.type";
-import LinkTag from "../LinkTag/LinkTag";
+import LinkTags from "../LinkTags/LinkTags";
 
 import "./LinkCard.css";
 
 type LinkCardProps = {
   link: Link;
+  removeable: boolean;
   onClick: () => void;
 };
-export default function LinkCard({ link, onClick }: LinkCardProps) {
+export default function LinkCard({ link, removeable, onClick }: LinkCardProps) {
+  const [showConfirmRemove, setShowConfirmRemove] = useState<boolean>(false);
+  useEffect(() => {
+    if (!removeable) {
+      setShowConfirmRemove(false);
+    }
+  }, [removeable]);
   return (
-    <div className="link-card" onClick={onClick}>
-      <div className="link-card-text">{link.name}</div>
-      <div className="link-card-tags">
-        {link.tags.map((tag, ind) => {
-          if (ind > 4) {
-            return;
-          }
-          return <LinkTag key={`link-tag-${ind}`} name={tag} ind={ind} />;
-        })}
+    <section
+      className={`link-card ${!removeable && "clickable-card"}`}
+      onClick={removeable ? () => null : onClick}
+    >
+      <div className="link-card-text">
+        {showConfirmRemove ? "Remove?" : link.name}
       </div>
-    </div>
+      {showConfirmRemove ? (
+        <div className="removal-confirmation-buttons-row">
+          <div className="confirm-removal-button">
+            <IconCheck size={24} />
+          </div>
+          <div
+            className="confirm-removal-button"
+            onClick={() => setShowConfirmRemove(false)}
+          >
+            <IconX size={24} />
+          </div>
+        </div>
+      ) : (
+        <LinkTags tags={link.tags} />
+      )}
+      {removeable && (
+        <div
+          className="remove-card-button"
+          onClick={() => setShowConfirmRemove(true)}
+          tabIndex={0}
+        >
+          <IconX size={18} />
+        </div>
+      )}
+    </section>
   );
 }
